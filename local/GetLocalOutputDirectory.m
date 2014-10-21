ddb24eaa9786973f93a231062134a69210e5d76d
Matt Kelsey <kelseym@mir.wustl.edu>
2014-10-21 10:38:16 -0500

    Added "local" output directory finder

---
+++ local/GetLocalOutputDirectory.m
@@ -0,0 +1,9 @@
+%% Return output directory on the local file system
+%  This file should be edited to point to a local directory
+%  Changes to this file should not be checked into the repository
+
+function outputDirectory = GetLocalOutputDirectory()
+
+outputDirectory = '/Users/Kelsey/Projects/EON/eon_microstates/output/';
+
+end
