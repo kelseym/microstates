f030cffb4dd21ea9b8923bf7fa838a33b11e29f9
Matt Kelsey <kelseym@mir.wustl.edu>
2014-10-21 10:27:59 -0500

    Added functions to "local" directory.  These functions should be edited by each user to point to local files and directories.  Changes to these files should not be checked-in.

---
+++ local/GetLocalDataFile.m
@@ -0,0 +1,9 @@
+%% Return data file (with path) on the local file system
+%  This .m file should be edited to point to a local directory
+%  Changes to this file should not be checked into the repository
+
+function dataFile = GetLocalDataFile()
+
+dataFile = '/Users/Kelsey/Projects/EON/MEG 20 subjects/hcp_microstate_data_restin/105923_MEG_3-Restin_rmegpreproc.mat';
+
+end
