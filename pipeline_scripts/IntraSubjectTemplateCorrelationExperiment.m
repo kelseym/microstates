713b5dddc8a60d6552af7d3ced5e92d63dfa4b3b
Matthew Kelsey <kelseym@mir.wustl.edu>
2014-12-11 17:08:38 -0600

    Added experiment to compare intra-scan template sets

---
+++ pipeline_scripts/IntraSubjectTemplateCorrelationExperiment.m
@@ -0,0 +1,97 @@
+%% Plot sorted microstate template maps for a single subject while varing the number of allowed microstates
+
+clear;
+
+
+fileName = GetLocalDataFile();
+outputDir = GetLocalOutputDirectory();
+microstatesRange = [2,4,7,10];
+
+% select and open preprocessed HCP MEG data file
+load(fileName, 'data');
+
+% band filter preprocess
+cfg = [];
+cfg.continuous = 'yes';
+cfg.bsfilter = 'yes';
+cfg.bsfreq = [59 61; 119 121; 179 181];
+cfg.demean = 'yes';
+cfg.detrend = 'yes';
+cfg.bpfilter = 'yes';
+cfg.bpfreq = [1.0 40.0];
+data = ft_preprocessing(cfg, data);
+
+data = ConcatenateTrials(data);
+
+%% extract N microstate templates
+dataStructs={};
+for numMicrostates=microstatesRange;
+  % extract N microstate templates
+  cfg = [];
+  cfg.numtemplates = numMicrostates;
+  cfg.datastructs = data;
+  cfg.clustertrainingstyle = 'global';
+  microstateTemplates = ExtractMicrostateTemplates(cfg);
+  data.microstateTemplates = microstateTemplates{1};
+  
+  dataStructs{end+1} = data;
+end
+
+
+%% Find sensors that exist in all scans
+sensorLabelIntersection = dataStructs{1}.label;
+for scni=2:length(dataStructs)
+  sensorLabelIntersection = intersect(sensorLabelIntersection, dataStructs{scni}.label);
+end
+
+%% Open layout
+cfg = [];
+cfg.layout = '4D248.mat';
+lay = ft_prepare_layout(cfg);
+
+%% Plot N microstate set with correlation to N-1 set
+nTemplateResortIndx = [];
+for scni=2:length(dataStructs)
+  nMinusOneMicrostateData = dataStructs{scni-1};
+  nMicrostateData = dataStructs{scni};
+  [a,~] = match_str(nMinusOneMicrostateData.label, sensorLabelIntersection);
+  nMinusOneMicrostateTemplates = nMinusOneMicrostateData.microstateTemplates{1}(:,a);
+  % this insures that the order of subsequent template plots remain sorted
+  if length(nTemplateResortIndx) == size(nMinusOneMicrostateTemplates,1)
+    nMinusOneMicrostateTemplates = nMinusOneMicrostateTemplates(nTemplateResortIndx,:);
+  end
+  [a,~] = match_str(nMicrostateData.label, sensorLabelIntersection);
+  nMicrostateTemplates = nMicrostateData.microstateTemplates{1}(:,a);
+  % similarityMatrix holds corrilation between ith nMinusOneMicrostateTemplates and jth nMicrostateTemplates
+  similarityMatrix = TemplateSetCorrelation(nMinusOneMicrostateTemplates, nMicrostateTemplates);
+  % Build a figure which shows nMinusOne templates with their highly correlated counterparts in the n set
+  topoFigureH = figure;
+  subplot(2,max(size(nMinusOneMicrostateTemplates,1), size(nMicrostateTemplates,1)), 1);
+  % compute the number of matches to the ith template
+  [~,maxCorrIndx]=max(similarityMatrix,[],1);
+  % Plot nMinusOneTemplates in first row
+  for i=1:size(nMinusOneMicrostateTemplates,1)
+    colStart = nnz(maxCorrIndx<i)+1;
+    colEnd = colStart + nnz(maxCorrIndx==i) - 1;
+    subplot(2,max(size(nMinusOneMicrostateTemplates,1), size(nMicrostateTemplates,1)), colStart:colEnd);
+    PlotMicrostateTemplate(nMinusOneMicrostateTemplates(i,:), nMinusOneMicrostateData.label, lay);
+  end
+  % Plot nTemplates under highest correlated nMinusOneTemplate
+  [~,nTemplateResortIndx] = sort(maxCorrIndx);
+  for i=1:size(nMicrostateTemplates,1)
+    tmpltIndx = nTemplateResortIndx(i);
+    subplot(2,max(size(nMinusOneMicrostateTemplates,1), size(nMicrostateTemplates,1)), max(size(nMinusOneMicrostateTemplates,1), size(nMicrostateTemplates,1))+i);
+    PlotMicrostateTemplate(nMicrostateTemplates(tmpltIndx,:), nMicrostateData.label, lay);
+    %title(sprintf('%1.2F', similarityMatrix
+  end
+  
+  
+end
+
+
+
+
+
+
+
+
