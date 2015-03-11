b0dde306f94ece143f1c8617bb34717c0d3d8e33
Matt Kelsey <kelseym@mir.wustl.edu>
2015-03-10 23:09:59 -0500

    Renamed some bps scripts and updated launcher script

--- bin/launchEonMicrostates.sh
+++ bin/launchEonMicrostates.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 LOGDIR=/home/kelseym/eon/pipeline_logs
 QSUBDIR=/home/kelseym/eon/eon_microstates/bin/
-PIPELINE="BandLimitedTopographyStability"
+#PIPELINE="BandLimitedTopographyStability"
+PIPELINE="RegionalBandLimitedGEV"
 #-------------------------------------------------------------- 
 
-qsub -V -q dque -l walltime=23:59:59,mem=24gb,vmem=24gb  -o $LOGDIR/OUTLOG_$PIPELINE_$EXPERIMENTID.log -e $LOGDIR/ERRORLOG_$PIPELINE_$EXPERIMENTID.log $QSUBDIR/band_limited_topography_stability.pbs > $LOGDIR/jobids-$PIPELINE_$EXPERIMENTID.txt
+qsub -V -q dque -l walltime=8:59:59,mem=24gb,vmem=24gb  -o $LOGDIR/OUTLOG_$PIPELINE_$EXPERIMENTID.log -e $LOGDIR/ERRORLOG_$PIPELINE_$EXPERIMENTID.log $QSUBDIR/$PIPELINE.pbs > $LOGDIR/jobids-$PIPELINE_$EXPERIMENTID.txt
 
 #-------------------------------------------------------------- 
