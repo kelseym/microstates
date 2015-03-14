6c80526b404c8055e802e0d10ef717c0176f0ceb
Matt Kelsey <kelseym@login001.(none)>
2015-03-14 13:29:18 -0500

    .

--- bin/launchRegionalBandLimitedGEV.sh
+++ bin/launchRegionalBandLimitedGEV.sh
@@ -8,11 +8,12 @@ PIPELINE="RegionalBandLimitedGEV"
 #-------------------------------------------------------------- 
 
 DATADIR="/BlueArc-scratch/kelseym/MEG_20_Subjects/hcp_microstate_data_restin"     # This is the directory containing MEG data to be processed
-DATAFILES=`find $DATADIR -name "*.mat"`;
+DATAFILES=`find $DATADIR -name "*3-Restin*.mat" -print0 | xargs --null -n1 basename`;
 for ff in $DATAFILES
 do
-  export EXPERIMENTID=${ff:2:19}
-  qsub -V -q dque -l walltime=8:59:59,mem=16gb,vmem=16gb  -o $LOGDIR/OUTLOG_$PIPELINE_$EXPERIMENTID.log -e $LOGDIR/ERRORLOG_$PIPELINE_$EXPERIMENTID.log $QSUBDIR/$PIPELINE.pbs > $LOGDIR/jobids-$PIPELINE_$EXPERIMENTID.txt
+  export EXPERIMENTID=${ff:0:19}
+  echo 'Launching' $EXPERIMENTID
+  qsub -V -q dque -l walltime=7:59:59,mem=12gb,vmem=12gb  -o $LOGDIR/OUTLOG_$PIPELINE_$EXPERIMENTID.log -e $LOGDIR/ERRORLOG_$PIPELINE_$EXPERIMENTID.log $QSUBDIR/$PIPELINE.pbs > $LOGDIR/jobids-$PIPELINE_$EXPERIMENTID.txt
 done
 
 #-------------------------------------------------------------- 
